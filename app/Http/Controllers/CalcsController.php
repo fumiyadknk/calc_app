8beb6ac3f196b7551a16cf519647dee2e313c3ea
fumiyadknk <fumiyak74@gmail.com>
2022-10-14 19:27:15 +0900

    課題修正

--- app/Http/Controllers/CalcsController.php
+++ app/Http/Controllers/CalcsController.php
@@ -6,22 +6,17 @@ use Illuminate\Http\Request;
 
 class CalcsController extends Controller
 {
-     public function calculate($n_1, $operator, $n_2)
-     {      
+    public function calculate($n_1, $operator, $n_2)
+    {
         if ($operator == "addition") {
             $result = $n_1 + $n_2;
-        } elseif ($operator == "subtractio"){
+        } elseif ($operator == "subtraction") {
             $result = $n_1 - $n_2;
-        } elseif ($operator == "multiplication"){
+        } elseif ($operator == "multiplication") {
             $result = $n_1 * $n_2;
-        } elseif ($operator == "division"){
+        } elseif ($operator == "division") {
             $result = $n_1 / $n_2;
         }
-        return view("calcs.result" , ["result" => $result]);
-
-     }
+        return view("calcs.result", ["result" => $result]);
+    }
 };
-
-
-
-
